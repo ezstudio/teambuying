47b248ffe64bd50b09a91bfc93578fde0d18f651
Johnson <johnson.ezmail@gmail.com>
2016-09-05 17:16:17 +0800

    加入專案檔案。

---
+++ teambuying/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace teambuying
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapPageRoute("", "home/{id}", "~/razor/home/home.cshtml");
+        }
+    }
+}
